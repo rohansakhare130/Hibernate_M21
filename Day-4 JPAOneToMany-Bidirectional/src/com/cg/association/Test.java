37a7db07b609d34e04c81ec4ae5c982f3104b56e
rohan <rohan@Rohan>
2022-09-07 15:58:00 +0530

    First commit

---
+++ Day-4 JPAOneToMany-Bidirectional/src/com/cg/association/Test.java
@@ -0,0 +1,40 @@
+package com.cg.association;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Test {
+
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		//create a new department
+		Department d = new Department();
+		d.setId(10);
+		d.setName("Accounting");
+		
+		Department d1 = new Department();
+		d1.setId(11);
+		d1.setName("It Manager");
+		
+		Employee e = new Employee();
+		e.setName("Vaibhav");
+		e.setSalary(40000);
+		e.setDept(d);
+		
+		Employee e1 = new Employee();
+		e1.setName("Rohan");
+		e1.setSalary(50000);
+		e1.setDept(d1);
+		
+		em.persist(e);
+		em.persist(e1);
+		
+		System.out.println("Add the Recored");
+		em.getTransaction().commit();
+		em.close();
+		factory.close();
+	}
+}
